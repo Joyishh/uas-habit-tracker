e4a3d34266a7cbc53a35daf1c211da5c69d7f0a7
Adrian Satrio <115604537+Joyishh@users.noreply.github.com>
2025-05-31 14:42:15 +0700

    Delete src directory

--- src/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllUser, getUserById } from '../controller/userController.js';
-
-const router = express.Router();
-
-router.get('/', getAllUser);
-router.get('/:id', getUserById);
-
-export default router;
